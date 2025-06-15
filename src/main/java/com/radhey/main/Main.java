2b617d3b733d25e4a7a68cb1b944f478479ec3f7
pancham01 <pancham9557@gmail.com>
2025-06-15 12:04:29 +0530

    update

--- src/main/java/com/radhey/main/Main.java
+++ src/main/java/com/radhey/main/Main.java
@@ -3,6 +3,10 @@ package com.radhey.main;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 
 import com.radhey.entity.Employee;
@@ -11,19 +15,32 @@ public class Main {
 	
 	public static Session getSession()
 	{
-		Configuration cfg = new Configuration().configure("hibernate.cfg.xml");
-		SessionFactory sf = cfg.buildSessionFactory();
-		return sf.openSession();
+//		Configuration cfg = new Configuration().configure();
+//		SessionFactory sf = cfg.buildSessionFactory();
+//		return new Configuration().configure().buildSessionFactory().openSession();
+		
+		
+//		StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure().build();
+//		Metadata metadata = new MetadataSources(ssr).getMetadataBuilder().build();
+//		SessionFactory sessionFactory = metadata.buildSessionFactory();
+		return new MetadataSources(new StandardServiceRegistryBuilder().configure().build()).getMetadataBuilder().build().buildSessionFactory().openSession();
 	}
 
 	public static void main(String[] args) {
-		Employee e = new Employee(2, "Radha", "female", 987650);
-		Session session = Main.getSession();
-		Transaction tx = session.beginTransaction();
 		
+		Employee e = new Employee(9, "Sukoon", "male", 987650);
+		
+		StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure().build();
+		Metadata metadata = new MetadataSources(ssr).getMetadataBuilder().build();
+		SessionFactory sessionFactory = metadata.buildSessionFactory();
+		
+		Session session = sessionFactory.openSession();
+		Transaction tx = session.beginTransaction();
 		
 		session.persist(e);
 		tx.commit();
+		session.close();
+		sessionFactory.close();
 		
 	}
 
