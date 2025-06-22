8af57225eae933c191814836e761aceee2cdd395
pancham01 <pancham9557@gmail.com>
2025-06-22 12:12:22 +0530

    update

---
+++ src/main/java/com/radhey/configuration/HibernateConfig.java
@@ -0,0 +1,41 @@
+package com.radhey.configuration;
+
+import java.util.Properties;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Environment;
+
+public class HibernateConfig {
+	
+	
+	
+	public static SessionFactory getSessionFactory()
+	{
+		Properties prop = new Properties();
+		
+		prop.put(Environment.DIALECT,"org.hibernate.dialect.MySQLDialect");
+		prop.put(Environment.JAKARTA_JDBC_URL,"jdbc:mysql://localhost:3306/testDb");
+		prop.put(Environment.JAKARTA_JDBC_USER,"root");
+		prop.put("jakarta.persistence.jdbc.password","root");
+		prop.put(Environment.HBM2DDL_AUTO,"update");
+		prop.put(Environment.SHOW_SQL,"true");
+		prop.put(Environment.FORMAT_SQL,"true");
+
+//		In XML based configuration		
+//		StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure().build();
+//		Metadata metadata = new MetadataSources(ssr).getMetadataBuilder().build();
+//		SessionFactory sessionFactory = metadata.buildSessionFactory();
+		
+//		In Java based configuration		
+//		StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().applySettings(prop).build();
+//		MetadataSources metaDataSources =	new MetadataSources(ssr).addAnnotatedClass(com.radhey.entity.Employee.class);
+//		SessionFactory sessionFactory = metaDataSources.getMetadataBuilder().build().buildSessionFactory();
+		
+		StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().applySettings(prop).build();
+		return new MetadataSources(ssr).addAnnotatedClass(com.radhey.entity.Employee.class).getMetadataBuilder().build().buildSessionFactory();
+	}
+
+}
--- src/main/java/com/radhey/main/Main.java
+++ src/main/java/com/radhey/main/Main.java
@@ -3,36 +3,18 @@ package com.radhey.main;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.hibernate.boot.Metadata;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
 
+import com.radhey.configuration.HibernateConfig;
 import com.radhey.entity.Employee;
 
 public class Main {
 	
-	public static Session getSession()
-	{
-//		Configuration cfg = new Configuration().configure();
-//		SessionFactory sf = cfg.buildSessionFactory();
-//		return new Configuration().configure().buildSessionFactory().openSession();
-		
-		
-//		StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure().build();
-//		Metadata metadata = new MetadataSources(ssr).getMetadataBuilder().build();
-//		SessionFactory sessionFactory = metadata.buildSessionFactory();
-		
-		return new MetadataSources(new StandardServiceRegistryBuilder().configure().build()).getMetadataBuilder().build().buildSessionFactory().openSession();
-	}
 
 	public static void main(String[] args) {
 		
-		Employee e = new Employee("Shanti", "female", 987650);
-		
-		Session session = Main.getSession();
-		
+		Employee e = new Employee("Muskan", "female", 987650);
+		SessionFactory sf = HibernateConfig.getSessionFactory();
+		Session session = sf.openSession();
 		Transaction tx = session.beginTransaction();
 		session.persist(e);
 		tx.commit();
